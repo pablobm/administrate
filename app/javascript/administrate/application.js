c667fd01d048d3e4dda0a52b035f5fc42ce671be
Elias Rodrigues <eliasrodrigues.info@gmail.com>
2023-07-06 21:44:49 -0300

    Fix jquery import due to hoisting

--- app/javascript/administrate/application.js
+++ app/javascript/administrate/application.js
@@ -1,9 +1,6 @@
-import jquery from "jquery";
-window.jQuery = jquery;
-window.$ = jquery;
+import "./add_jquery";
 
 import {} from "jquery-ujs";
-
 import "selectize/dist/js/selectize.min.js";
 
 import "./components/associative";
