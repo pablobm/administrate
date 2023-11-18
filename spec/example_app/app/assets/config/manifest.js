7a124bc0c6903061ab5e8a5ed6897d9687699e64
Pablo Brasero <pablo@pablobm.com>
2023-11-18 15:46:18 +0000

    Works on a production-like env with Rails 7

--- spec/example_app/app/assets/config/manifest.js
+++ spec/example_app/app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link administrate/application.css
 //= link administrate/application.js
+//= link administrate/components/table.js
+//= link administrate/components/select.js
+//= link administrate/components/associative.js
